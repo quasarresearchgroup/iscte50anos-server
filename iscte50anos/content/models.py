b1d6dd83413e3c3b532d2154ab4bb0eb1d4d2036
dsba <duarteuniversidade@gmail.com>
2022-09-10 01:58:01 +0100

    increase link size (content and event)

--- iscte50anos/content/models.py
+++ iscte50anos/content/models.py
@@ -4,7 +4,7 @@ from topics.models import Topic
 
 class Content(models.Model):
     title = models.CharField(max_length=300)
-    link = models.CharField(max_length=100, blank=True)
+    link = models.CharField(max_length=300, blank=True)
     topics = models.ManyToManyField(Topic, related_name='content')
 
     # date = models.DateField(null=True, blank=True)
