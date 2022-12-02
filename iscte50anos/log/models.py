ab5f8f279b228b45109eaa48e9a669a24a316f0a
dsba <duarteuniversidade@gmail.com>
2022-12-02 13:26:06 +0000

    add log str method

--- iscte50anos/log/models.py
+++ iscte50anos/log/models.py
@@ -5,3 +5,6 @@ class AccessLog(models.Model):
     origin_ip = models.CharField(max_length=15)
     link = models.CharField(max_length=100)
     access_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Access: {self.origin_ip} || Link: {self.link} || Time: {self.access_time}'
