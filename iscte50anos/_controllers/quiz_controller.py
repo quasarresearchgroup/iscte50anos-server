efd5c9eda21427a16f144eea02e1fe3c54c2b979
dsba <duarteuniversidade@gmail.com>
2023-03-20 01:50:14 +0000

    first question is self explanatory

--- iscte50anos/_controllers/quiz_controller.py
+++ iscte50anos/_controllers/quiz_controller.py
@@ -71,12 +71,7 @@ def assign_trial_questions(user, trial, topics):
     if len(first_questions) == 0:
         first_questions = list(Question.objects.filter(category="self_explanatory"))
 
-    questions = [random.choice(first_questions)]
-    print(middle_questions)
-    print(questions)
-    print(questions.append(random.sample(middle_questions, QUIZ_SIZE - 2)))
-
-    questions = questions.append(random.sample(middle_questions, QUIZ_SIZE - 2))
+    questions = [random.choice(first_questions), random.sample(middle_questions, QUIZ_SIZE - 2)]
 
     geo_questions = Question.objects.filter(topics__title="Georeferenciação") \
         .exclude(trial_questions__trial__quiz__user=user)
